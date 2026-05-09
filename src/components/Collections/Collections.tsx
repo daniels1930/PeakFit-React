6c9ad400053acc473ef4536acb249d38ddba4496
Daniels Arias <danielariasbedoya1901@gmail.com>
2026-05-08 20:35:42 -0500

    Add women collection page

--- src/components/Collections/Collections.tsx
+++ src/components/Collections/Collections.tsx
@@ -1,10 +1,11 @@
+import { Link } from "react-router-dom";
 import "./Collections.css";
 
 const collections = [
-  { title: "Men", image: "/assets/images/footer/Men.jpg" },
-  { title: "Woman", image: "/assets/images/footer/Woman.jpg" },
-  { title: "Accessories", image: "/assets/images/footer/accessories.jpg" },
-  { title: "Equipment", image: "/assets/images/footer/equipment.jpg" },
+  { title: "Men", image: "/assets/images/footer/Men.jpg", to: null },
+  { title: "Woman", image: "/assets/images/footer/Woman.jpg", to: "/collections/women" },
+  { title: "Accessories", image: "/assets/images/footer/accessories.jpg", to: null },
+  { title: "Equipment", image: "/assets/images/footer/equipment.jpg", to: null },
 ];
 
 function Collections() {
@@ -15,15 +16,29 @@ function Collections() {
       </h2>
 
       <div className="collections-container">
-        {collections.map((collection) => (
-          <article className="collection-card" key={collection.title}>
-            <img src={collection.image} alt={collection.title} />
-            <div className="overlay">
-              <h3>{collection.title}</h3>
-              <span className="line" />
-            </div>
-          </article>
-        ))}
+        {collections.map((collection) => {
+          const card = (
+            <article className="collection-card" key={collection.title}>
+              <img src={collection.image} alt={collection.title} />
+              <div className="overlay">
+                <h3>{collection.title}</h3>
+                <span className="line" />
+              </div>
+            </article>
+          );
+
+          return collection.to ? (
+            <Link
+              to={collection.to}
+              key={collection.title}
+              style={{ textDecoration: "none", color: "inherit" }}
+            >
+              {card}
+            </Link>
+          ) : (
+            card
+          );
+        })}
       </div>
     </section>
   );
